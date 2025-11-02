c19ebbaf155c7cc15cef279d9979880f40910f9b
crystalpoplar <brokenhearted1682@gmail.com>
2025-11-02 18:06:37 -0500

    Add function to retrieve values from JSON by keys
    Introduced getValuesFromJson to fetch values for given keys (including nested keys) from a JSON file using file_management.getJsonDict. This utility simplifies extracting multiple values from structured JSON data.

--- src/modules/object_management.py
+++ src/modules/object_management.py
@@ -1,3 +1,5 @@
+import file_management
+
 def make_list_comma_separated(list):
     """
     Converts a list to a comma-separated string.
@@ -48,3 +50,32 @@ def makeTextJson(textStr):
         textStr = textStr[:-1]
     return textStr
 
+def getValuesFromJson(keys, jsonFilePath):
+    """
+    Retrieves values from a JSON file based on the provided keys.
+
+    Parameters:
+        keys (list): A list of string keys to retrieve values for. Nested keys should be provided as lists.
+        jsonFilePath (str): The path to the JSON file.
+
+    Returns:
+        tuple: A tuple of values corresponding to the keys. If a key does not exist, the value will be None.
+    """
+    def get_nested_value(data, key_list):
+        """
+        Helper function to retrieve nested values from a dictionary.
+        """
+        for key in key_list:
+            if isinstance(data, dict):
+                data = data.get(key, None)
+            else:
+                return None
+        return data
+
+    # Load the JSON file
+    jsonData = file_management.getJsonDict(jsonFilePath)
+    
+    # Retrieve values for the keys, set to None if key does not exist
+    values = tuple(get_nested_value(jsonData, key) if isinstance(key, list) else jsonData.get(key, None) for key in keys)
+    
+    return values
